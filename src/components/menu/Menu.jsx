7ef8988a9291f311ace66f3dd56c13e0910558e1
Daniel Clements <clemies2000@hotmail.com>
2021-11-17 10:33:02 +0000

    Add interactive menu button in topbar

---
+++ src/components/menu/Menu.jsx
@@ -0,0 +1,13 @@
+import "./menu.scss"
+
+export default function Menu({ menuOpen, setMenuOpen }) {
+  return (
+    <div className={"menu " + (menuOpen && "active")} id="menu">
+      <ul>
+        <li><a href="#intro">Home</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </div>
+  )
+}
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,9 @@
 import "./topbar.scss"
 import {Mail,LinkedIn} from "@material-ui/icons"
 
-export default function Topbar() {
+export default function Topbar({ menuOpen, setMenuOpen }) {
   return (
-    <div className="topbar">
+    <div className={"topbar " + (menuOpen && "active")}>
       <div className="wrapper">
         <div className="left">
           <div className="name">
@@ -19,9 +19,11 @@ export default function Topbar() {
           </div>
         </div>
         <div className="right">
-          <a href="#intro">Intro</a>
-          <a href="#portfolio">Portfolio</a>
-          <a href="#contact">Contact</a>
+          <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
         </div>
       </div>
     </div>
