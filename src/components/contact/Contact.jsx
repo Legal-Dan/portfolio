7b2cb0a2dd52ab214f239fdb6d3da61b252a06a2
Daniel Clements <clemies2000@hotmail.com>
2021-11-19 15:11:52 +0000

    Add basic contact page

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,22 @@
 import "./contact.scss"
+import {Mail,LinkedIn,GitHub} from "@material-ui/icons"
 
 export default function Contact() {
   return (
     <div className="contact" id="contact">
-      You can contact me at:
+      <h1>You can contact me at:</h1>
+      <div className="itemContainer">
+            <LinkedIn className="icon"/>
+            <span><a href="https://www.linkedin.com/in/daniel-clements-10878ba8/">LinkedIn Profile</a></span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon"/>
+            <span><a href="mailto:clemies2000@hotmail.com">clemies2000@hotmail.com</a></span>
+          </div>
+          <div className="itemContainer">
+            <GitHub className="icon"/>
+            <span><a href="https://github.com/Legal-Dan">GitHub Profile</a></span>
+          </div>
     </div>
   )
 }
